dc3e68d350386236d0e94562d39d4a5e040939f9
Sinha5714 <>
2023-06-07 16:24:51 +0000

    Comment model: Add comment model class for comment by user for event

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from events.models import Event
 
-# Create your models here.
+
+class Comment(models.Model):
+    """
+    Comment model for adding comments on Events, related to Event
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField(blank=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return 'comment on {} by {}'.format(self.event,
+                                            self.user.username)
