43c49377e030fd449faadcdc690c3bc4a846c762
Sinha5714 <>
2023-06-07 11:25:32 +0000

    EventDetail: Create eventdetail class for owner of event to edit and delete the event

--- events/views.py
+++ events/views.py
@@ -13,3 +13,16 @@ class EventList(generics.ListAPIView):
     serializer_class = EventSerializer
     queryset = Event.objects.annotate(
     ).order_by('-created_on')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve an event, or update or delete it by id if you own it.
+    """
+    serializer_class = EventSerializer
+    permission_classes = [IsUserOrReadOnly]
+    queryset = Event.objects.annotate(
+    ).order_by('-created_on')
