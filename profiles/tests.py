edcb9ac7019ad0950881166a7bdd7bb5a10b207d
Sinha5714 <>
2023-06-07 11:49:30 +0000

    Testing: Done automated testing for ProfileListView and pass all test

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProfileListViewTests(APITestCase):
+    """
+    Tests for the Profile model list view
+    """
+
+    def setUp(self):
+        user1 = User.objects.create_user(username='user1', password='pass1')
+        user2 = User.objects.create_user(username='user2', password='pass2')
+        user3 = User.objects.create_user(username='user3', password='pass3')
+
+    def test_profile_automatically_created_on_user_creation(self):
+        response = self.client.get('/profiles/')
+        count = Profile.objects.count()
+        self.assertEqual(count, 3)
+
+    def test_can_list_profiles(self):
+        response = self.client.get('/profiles/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
