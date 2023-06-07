10d85ffe17ec09b38fce102a047ff11d88b1e9c4
Sinha5714 <>
2023-06-07 12:01:17 +0000

    Testing: Done automated testing for unable to retrieve a profile with invalid id

--- profiles/tests.py
+++ profiles/tests.py
@@ -37,3 +37,7 @@ class ProfileDetailViewTests(APITestCase):
     def test_can_retrieve_profile_using_valid_id(self):
         response = self.client.get('/profiles/2/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_retrieve_profile_using_invalid_id(self):
+        response = self.client.get('/profiles/4/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
