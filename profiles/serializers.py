3d40fa47989f05bf95aa0e2ea9a72618345b73b5
Sinha5714 <>
2023-06-10 11:18:38 +0000

    Add get_following_id function in profiles.serializers.py and add update filterset fields in views.py

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -8,15 +9,29 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
     user = serializers.ReadOnlyField(source='user.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    events_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.user
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                user=user, followed=obj.user
+            ).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
         fields = [
             'id', 'user', 'is_owner', 'created_on', 'updated_on', 'name',
             'about_me', 'instagram_link', 'facebook_link',
-            'phone_number', 'email', 'profile_pic',
+            'phone_number', 'email', 'profile_pic', 'following_id',
+            'events_count', 'followers_count', 'following_count',
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -14,6 +14,8 @@ class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.annotate(
         events_count=Count('user__event', distinct=True),
+        followers_count=Count('user__followed', distinct=True),
+        following_count=Count('user__following', distinct=True),
     ).order_by('-created_on')
 
     filter_backends = [
@@ -21,10 +23,15 @@ class ProfileList(generics.ListAPIView):
         DjangoFilterBackend,
     ]
     filterset_fields = [
-        'user__profile'
+        'user__following__followed__profile',
+        'user__followed__user__profile',
     ]
     ordering_fields = [
         'events_count',
+        'followers_count',
+        'following_count',
+        'user__following__created_on',
+        'user__followed__created_on',
     ]
 
 
@@ -35,4 +42,7 @@ class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ProfileSerializer
     permission_classes = [IsUserOrReadOnly]
     queryset = Profile.objects.annotate(
+        events_count=Count('user__event', distinct=True),
+        followers_count=Count('user__followed', distinct=True),
+        following_count=Count('user__following', distinct=True),
     ).order_by('-created_on')
