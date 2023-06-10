01828894f4c62a65aea8992ea1f66472a6f8fd3c
Sinha5714 <>
2023-06-10 08:51:17 +0000

    Interested: Create interested model for user to add interest to event

--- interested/models.py
+++ interested/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from events.models import Event
 
-# Create your models here.
+
+class Interested(models.Model):
+    """
+    Class model for Interested, related to user and events
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Event, related_name='interested', on_delete=models.CASCADE
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.user} {self.event}'
