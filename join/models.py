60a47fa52ee85709c3c3494cbd9f51fcdd2abd0f
Sinha5714 <>
2023-06-11 18:36:45 +0000

    Remove profile from Join model

--- join/models.py
+++ join/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from profiles.models import Profile
 from events.models import Event
 
 
@@ -9,7 +8,6 @@ class Join(models.Model):
     A class for the Join model
     """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     reason = models.TextField(max_length=255)
@@ -18,4 +16,4 @@ class Join(models.Model):
         ordering = ['-created_on']
 
     def __str__(self):
-        return f'{self.profile.name} for {self.event.title} : {self.reason}'
+        return f'{self.user} for {self.event.title} : {self.reason}'
