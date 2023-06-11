8effc0ae7b6aa7a10885bb2b2595e3a8aad385d3
Sinha5714 <>
2023-06-11 18:29:01 +0000

    Join: Create join model for user to add reason for joining an event

--- join/models.py
+++ join/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from profiles.models import Profile
+from events.models import Event
 
-# Create your models here.
+
+class Join(models.Model):
+    """
+    A class for the Join model
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    reason = models.TextField(max_length=255)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.profile.name} for {self.event.title} : {self.reason}'
--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'comments',
     'interested',
     'followers',
+    'join',
 ]
 
 MIDDLEWARE = [
