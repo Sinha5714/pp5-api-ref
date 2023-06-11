9d3abb537f1f7ab25ed5829e457775c7aafc5221
Sinha5714 <>
2023-06-11 20:02:06 +0000

    Add natural time format for comments created_on and updated on

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -12,10 +13,18 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(
         source='user.profile.profile_pic.url'
     )
+    created_on = serializers.SerializerMethodField()
+    updated_on = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.user
+
+    def get_created_on(self, obj):
+        return naturaltime(obj.created_on)
+
+    def get_updated_on(self, obj):
+        return naturaltime(obj.updated_on)
 
     class Meta:
         model = Comment
