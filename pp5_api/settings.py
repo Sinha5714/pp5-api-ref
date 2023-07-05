f5a6b521c23d52f830c07998f29aade7acc30cfa
Sinha5714 <>
2023-07-05 16:39:33 +0000

    Complete documentation and remove codeanywhere allowed host in settings.py for submission

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -64,8 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost',
-                 '8000-sinha5714-pp5-api-ref-k3h1ifqb7e.us2.codeanyapp.com']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost']
 
 
 # Application definition
